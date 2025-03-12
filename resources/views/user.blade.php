99e3919507551e66c72edaf37fac386f3525b560
Akmalz26 <akmalzcybergaming026@gmail.com>
2025-03-12 13:34:54 +0700

    test

--- resources/views/user.blade.php
+++ resources/views/user.blade.php
@@ -8,7 +8,7 @@
         <h2 class="mb-3">Selamat Datang!</h2>
         <p class="text-muted">Anda saat ini masuk sebagai <strong>guest</strong>.</p>
         <a href="{{ route('login') }}" class="btn btn-primary w-100">
-            <i class="fa-solid fa-sign-in-alt"></i> Login jika anda adalah Admin
+            <i class="fa-solid fa-sign-in-alt"></i> Login jika anda adalah Admin.
         </a>
     </div>
 </div>
